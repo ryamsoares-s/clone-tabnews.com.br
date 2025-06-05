90cce33fd9d8fe5e81085e9b2ccb0b428faa841f
ryamsoares-s <play.ryam@gmail.com>
2025-06-05 11:29:15 -0400

    atualiza função de status para executar consulta ao banco de dados e retorna resposta JSON

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,11 @@
-function status(request, response) { // 
+import database from "../../../../infra/database.js"; // Importing the database connection
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 +1 as soma;");
+  console.log(result.rows);
+
   response.status(200).json({
-    status: 'ok',
-    message: 'API is running smoothly',
+    status: "ok",
   });
 }
 
