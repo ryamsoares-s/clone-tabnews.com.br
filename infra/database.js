cef09a77de9e53b615c6063d33847d2791f00852
ryamsoares-s <play.ryam@gmail.com>
2025-06-11 11:42:00 -0400

    refatora função de consulta no banco de dados para melhorar o tratamento de erros

--- infra/database.js
+++ infra/database.js
@@ -2,16 +2,25 @@ import { Client } from "pg";
 
 async function query(queryObject) {
   const client = new Client({
-    host: process.env.POSTGRES_HOST, // CRIANDO AQUI A VARIÁVEL DE AMBIENTE
-    port: process.env.POSTGRES_PORT, // CRIANDO AQUI A VARIÁVEL DE AMBIENTE
-    user: process.env.POSTGRES_USER, // CRIANDO AQUI A VARIÁVEL DE AMBIENTE
-    database: process.env.POSTGRES_DB, // CRIANDO AQUI A VARIÁVEL DE AMBIENTE
-    password: process.env.POSTGRES_PASSWORD, // CRIANDO AQUI A VARIÁVEL DE AMBIENTE
+    host: process.env.POSTGRES_HOST, // ACESSANDO A VARIÁVEL DE AMBIENTE
+    port: process.env.POSTGRES_PORT, // ACESSANDO A VARIÁVEL DE AMBIENTE
+    user: process.env.POSTGRES_USER, // ACESSANDO A VARIÁVEL DE AMBIENTE
+    database: process.env.POSTGRES_DB, // ACESSANDO A VARIÁVEL DE AMBIENTE
+    password: process.env.POSTGRES_PASSWORD, // ACESSANDO A VARIÁVEL DE AMBIENTE
   });
   await client.connect();
-  const result = await client.query(queryObject);
-  await client.end();
-  return result;
+
+  try {
+    // Tenta executar a consulta no banco de dados
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    // Se ocorrer um erro, ele será capturado aqui
+    console.error("Database query error:", error);
+  } finally {
+    // Independentemente de ter ocorrido um erro ou não, o cliente será desconectado
+    await client.end();
+  }
 }
 
 export default {
