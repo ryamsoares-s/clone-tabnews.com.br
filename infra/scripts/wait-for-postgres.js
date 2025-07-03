15e4731287661c0f6074a6b4ca89a8e1c1ecfa90
ryamsoares-s <play.ryam@gmail.com>
2025-07-03 17:57:04 -0400

    Melhora a exibição do status de conexão do Postgres com animação e mensagem mais clara

--- infra/scripts/wait-for-postgres.js
+++ infra/scripts/wait-for-postgres.js
@@ -1,18 +1,25 @@
 const { exec } = require("node:child_process");
 
+let spinner = ["/", "-", "\\", "|"];
+let i = 0;
+
 function checkPostgres() {
   exec("docker exec postgres-dev pg_isready --host localhost", handleReturn);
 
   function handleReturn(error, stdout) {
     if (stdout.search("accepting connections") === -1) {
-      process.stdout.write(".");
+      process.stdout.clearLine(0);
+      process.stdout.cursorTo(0);
+      process.stdout.write(
+        `🔴 Aguardando Postgres aceitar conexões ${spinner[i++ % spinner.length]}`,
+      );
       checkPostgres();
       return;
     }
 
-    console.log("\n🟢 Postgres está pronto para conexões!\n");
+    console.log("\n🟢 Postgres está pronto e aceitando conexões!\n");
   }
 }
 
-process.stdout.write("\n\n🔴 Aguardando o Postgres aceitar conexões");
+process.stdout.write("🔴 Aguardando Postgres aceitar conexões ");
 checkPostgres();
