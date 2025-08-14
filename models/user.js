f4e943b08eaa20fb8d955aab61e8b0fd136e0424
ryamsoares-s <play.ryam@gmail.com>
2025-08-14 14:38:49 -0400

    feat: add support for `PATCH` on `/api/v1/users/[username]`

--- models/user.js
+++ models/user.js
@@ -34,60 +34,12 @@ async function findOneByUsername(username) {
 }
 
 async function create(userInputValues) {
-  await validateUniqueEmail(userInputValues.email);
   await validateUniqueUsername(userInputValues.username);
+  await validateUniqueEmail(userInputValues.email);
   await hashPasswordInObject(userInputValues);
 
   const newUser = await runInsertQuery(userInputValues);
   return newUser;
-
-  async function validateUniqueUsername(username) {
-    await database.query("CREATE EXTENSION IF NOT EXISTS unaccent;"); // Remover do model em refatoração futura
-    const results = await database.query({
-      text: `
-      SELECT 
-        username 
-      FROM 
-        users 
-      WHERE 
-        unaccent(LOWER(username)) = unaccent(LOWER($1))
-      ;`,
-      values: [username],
-    });
-
-    if (results.rowCount > 0) {
-      throw new ValidationError({
-        message: "O username já está sendo utilizado.",
-        action: "Utilize outro username para realizar o cadastro.",
-      });
-    }
-  }
-
-  async function validateUniqueEmail(email) {
-    const results = await database.query({
-      text: `
-      SELECT 
-        email 
-      FROM 
-        users 
-      WHERE 
-        LOWER(email) = LOWER($1)
-      ;`,
-      values: [email],
-    });
-
-    if (results.rowCount > 0) {
-      throw new ValidationError({
-        message: "O email já está sendo utilizado.",
-        action: "Utilize outro email para realizar o cadastro.",
-      });
-    }
-  }
-
-  async function hashPasswordInObject(userInputValues) {
-    const hashedPassword = await password.hash(userInputValues.password);
-    userInputValues.password = hashedPassword;
-  }
 
   async function runInsertQuery(userInputValues) {
     const results = await database.query({
@@ -110,9 +62,106 @@ async function create(userInputValues) {
   }
 }
 
+async function hashPasswordInObject(userInputValues) {
+  const hashedPassword = await password.hash(userInputValues.password);
+  userInputValues.password = hashedPassword;
+}
+
+async function validateUniqueEmail(email) {
+  const results = await database.query({
+    text: `
+      SELECT 
+        email 
+      FROM 
+        users 
+      WHERE 
+        LOWER(email) = LOWER($1)
+      ;`,
+    values: [email],
+  });
+
+  if (results.rowCount > 0) {
+    throw new ValidationError({
+      message: "O email já está sendo utilizado.",
+      action: "Utilize outro email para realizar esta operação.",
+    });
+  }
+}
+
+async function validateUniqueUsername(username) {
+  await database.query("CREATE EXTENSION IF NOT EXISTS unaccent;"); // Remover do model em refatoração futura
+  const results = await database.query({
+    text: `
+      SELECT 
+        username 
+      FROM 
+        users 
+      WHERE 
+        unaccent(LOWER(username)) = unaccent(LOWER($1))
+      ;`,
+    values: [username],
+  });
+
+  if (results.rowCount > 0) {
+    throw new ValidationError({
+      message: "O username já está sendo utilizado.",
+      action: "Utilize outro username para realizar esta operação.",
+    });
+  }
+}
+
+async function update(username, userInputValues) {
+  const currentUser = await findOneByUsername(username);
+
+  if ("username" in userInputValues) {
+    if (username.toLowerCase() !== userInputValues.username.toLowerCase())
+      await validateUniqueUsername(userInputValues.username);
+  }
+
+  if ("email" in userInputValues) {
+    await validateUniqueEmail(userInputValues.email);
+  }
+
+  if ("password" in userInputValues) {
+    await hashPasswordInObject(userInputValues);
+  }
+
+  const userWithNewValues = { ...currentUser, ...userInputValues };
+
+  const updateUser = await runUpdateQuery(userWithNewValues);
+  return updateUser;
+
+  async function runUpdateQuery(userWithNewValues) {
+    const results = await database.query({
+      text: `
+      UPDATE
+        users
+      SET
+        username = $2,
+        email = $3,
+        password = $4,
+        updated_at = timezone('utc', now())
+      WHERE
+        id = $1 
+      RETURNING
+        *
+      ;`,
+      values: [
+        userWithNewValues.id,
+        userWithNewValues.username,
+        userWithNewValues.email,
+        userWithNewValues.password,
+      ],
+    });
+
+    return results.rows[0];
+  }
+}
+
 const user = {
   create,
   findOneByUsername,
+  update,
 };
 
 export default user;
--- pages/api/v1/users/[username]/index.js
+++ pages/api/v1/users/[username]/index.js
@@ -5,6 +5,7 @@ import user from "models/user.js"; // Importa o modelo de usuário
 const router = createRouter();
 
 router.get(getHandler); // Define a rota GET para buscar um usuário
+router.patch(patchHandler); // Define a rota PATCH para atualizar um usuário
 
 export default router.handler(controller.errorHandlers); // Exporta o manipulador de erros e rota não encontrada
 
@@ -12,4 +13,12 @@ async function getHandler(request, response) {
   const username = request.query.username;
   const userFound = await user.findOneByUsername(username); // Busca o usuário pelo username
   return response.status(200).json(userFound);
+}
+
+async function patchHandler(request, response) {
+  const username = request.query.username;
+  const userInputValues = request.body;
+
+  const updateUser = await user.update(username, userInputValues); // Atualiza o usuário com os valores fornecidos
+  return response.status(200).json(updateUser);
 }
